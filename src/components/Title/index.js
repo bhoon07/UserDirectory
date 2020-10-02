e88426d950a881243ced784cbc76df93c76fed33
Blaine Hoon <bhoon07@gmail.com>
2020-10-02 13:11:22 -0500

    updates

--- src/components/Title/index.js
+++ src/components/Title/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./style.css";
 
 function Title(props) {
-  return <h1 className="title">Employee List</h1>;
+  return <h1 className="title">Employees List</h1>;
 }
 
 export default Title;
