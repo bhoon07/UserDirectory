4508016f575219fe6ade488b8821662b74fb9855
Blaine Hoon <bhoon07@gmail.com>
2020-10-02 10:27:47 -0500

    updates

--- src/components/EmployeeCard/index.js
+++ src/components/EmployeeCard/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./style.css";
 
-function FriendCard(props) {
+function EmployeeCard(props) {
   return (
     <div className="card">
       <div className="img-container">
@@ -24,4 +24,4 @@ function FriendCard(props) {
   );
 }
 
-export default FriendCard;
+export default EmployeeCard;
